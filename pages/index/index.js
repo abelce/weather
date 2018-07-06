46e6917921924de22d0451ed21ab2912921e7eb4
zxtang <zxtang@varitrip.com>
2018-07-06 13:09:03 +0800

    first push

---
+++ pages/index/index.js
@@ -0,0 +1,82 @@
+var bmap = require('../../libs/bmap-wx.js');
+Page({
+  data: {
+    weatherData: '',
+    bcgColor: '#40a7e7',
+    cityDates: {},
+  },
+  onLoad: function () {
+    var that = this;
+    // 新建百度地图对象 
+    var BMap = new bmap.BMapWX({
+      ak: 'GfyiBoolKtoBUXOdIibIDrAhBVOTy2GW'
+    });
+    var fail = function (data) {
+      console.log(data)
+    };
+    var success = function (data) {
+      console.log(data)
+      var weatherData = data.currentWeather[0];
+      weatherData = '城市：' + weatherData.currentCity + '\n' + 'PM2.5：' + weatherData.pm25 + '\n' + '日期：' + weatherData.date + '\n' + '温度：' + weatherData.temperature + '\n' + '天气：' + weatherData.weatherDesc + '\n' + '风力：' + weatherData.wind + '\n';
+      that.setData({
+        cityDates: data.originalData,
+      });
+    }
+    // 发起weather请求 
+    BMap.weather({
+      fail: fail,
+      success: success
+    });
+  },
+  calcPM: (pm) => {
+    if (value > 0 && value <= 50) {
+      return {
+        val: value,
+        desc: '优',
+        detail: '',
+      }
+    } else if (value > 50 && value <= 100) {
+      return {
+        val: value,
+        desc: '良',
+        detail: '',
+      }
+    } else if (value > 100 && value <= 150) {
+      return {
+        val: value,
+        desc: '轻度污染',
+        detail: '对敏感人群不健康',
+      }
+    } else if (value > 150 && value <= 200) {
+      return {
+        val: value,
+        desc: '中度污染',
+        detail: '不健康',
+      }
+    } else if (value > 200 && value <= 300) {
+      return {
+        val: value,
+        desc: '重度污染',
+        detail: '非常不健康',
+      }
+    } else if (value > 300 && value <= 500) {
+      return {
+        val: value,
+        desc: '严重污染',
+        detail: '有毒物',
+      }
+    } else if (value > 500) {
+      return {
+        val: value,
+        desc: '爆表',
+        detail: '能出来的都是条汉子',
+      }
+    }
+  },
+  success: (data) => {
+    wx.stopPullDownRefresh();
+    this.setData({
+      cityDates: data.originalData,
+    });
+  }
+})
